2f75ca2291ca33dffb2d6250b555b9ef02fc5cf7
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-12-09 09:59:47 -0800

    finishing 2. starting 3.

--- scripts/scripts/ep02.r
+++ scripts/scripts/ep02.r
@@ -46,3 +46,50 @@ mean(with_na) # mean() function cannot interpret the missing values
 # ignoring the missing values.
 mean(with_na, na.rm = TRUE)
 
+is.na(with_na) # This will produce a vector of logical values,
+
+# you can count logicals with sum()
+sum(is.na(with_na))
+
+
+# to identify the values that are not missing we write the following
+!is.na(with_na) # The ! operator means negation, i.e. not is.na(with_na)
+
+# this is rather esoteric:
+without_na <- with_na[!is.na(with_na)] # this notation will return only
+# the elements that have TRUE on their respective positions
+
+without_na
+
+# the point is to show the square bracket [] notation.
+with_na[3]
+
+
+
+# Factors
+nordic_str <- c("Norway", "Sweden", "Norway", "Denmark", "Sweden")
+nordic_str # regular character vectors printed out
+str(nordic_str)
+
+# factor() function converts a vector to factor data type
+nordic_cat <- factor(nordic_str)
+nordic_cat # With factors, R prints out additional information - 'Levels'
+
+str(nordic_cat)
+
+levels(nordic_cat) # returns all levels of a factor vector.
+nlevels(nordic_cat)
+
+# R will want to make them alphabetic. You can force the order:
+nordic_cat <- factor(
+  nordic_cat,
+  levels = c(
+    "Norway",
+    "Denmark",
+    "Sweden"
+  )
+)
+
+# now Norway will be the first category, Denmark second and Sweden third
+nordic_cat
+
